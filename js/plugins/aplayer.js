4d1e26b1f3092704723c8924feb7e9bb80b4e232
p0l1st <p0l1st@foxmail.com>
2025-04-25 12:36:56 +0800

    Site updated: 2025-04-25 12:36:34

--- js/plugins/aplayer.js
+++ js/plugins/aplayer.js
@@ -1,33 +1,31 @@
-(function() {
-  const audioList = [];
-  const isFixed = theme.plugins.aplayer.type === "fixed";
-  const isMini = theme.plugins.aplayer.type === "mini";
+const audioList = [];
+const isFixed = theme.plugins.aplayer.type == "fixed";
+const isMini = theme.plugins.aplayer.type == "mini";
 
-  for (const audio of theme.plugins.aplayer.audios) {
-    const audioObj = {
-      name: audio.name,
-      artist: audio.artist,
-      url: audio.url,
-      cover: audio.cover,
-      lrc: audio.lrc,
-      theme: audio.theme,
-    };
-    audioList.push(audioObj);
-  }
+for (const audio of theme.plugins.aplayer.audios) {
+  const a = {
+    name: audio.name,
+    artist: audio.artist,
+    url: audio.url,
+    cover: audio.cover,
+    lrc: audio.lrc,
+    theme: audio.theme,
+  };
+  audioList.push(a);
+}
 
-  if (isMini) {
-    new APlayer({
-      container: document.getElementById("aplayer"),
-      mini: true,
-      audio: audioList,
-    });
-  } else if (isFixed) {
-    const player = new APlayer({
-      container: document.getElementById("aplayer"),
-      fixed: true,
-      lrcType: 3,
-      audio: audioList,
-    });
-    document.querySelector(".aplayer-icon-lrc").click();
-  }
-})();
+if (isMini) {
+  const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    mini: true,
+    audio: audioList,
+  });
+} else if (isFixed) {
+  const ap = new APlayer({
+    container: document.getElementById("aplayer"),
+    fixed: true,
+    lrcType: 3,
+    audio: audioList,
+  });
+  document.querySelector(".aplayer-icon-lrc").click();
+}
